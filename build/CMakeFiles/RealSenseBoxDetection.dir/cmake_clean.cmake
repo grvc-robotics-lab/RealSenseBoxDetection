74d71b2d979401188a34c6baf8c574a699eaf695
Jorge Pozas Guerra <indianajorge0009@gmail.com>
2024-10-09 13:25:39 +0200

    Code with english comments

---
+++ build/CMakeFiles/RealSenseBoxDetection.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RealSenseBoxDetection.dir/src/RealSenseBoxDetection.cpp.o"
+  "CMakeFiles/RealSenseBoxDetection.dir/src/RealSenseBoxDetection.cpp.o.d"
+  "RealSenseBoxDetection"
+  "RealSenseBoxDetection.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RealSenseBoxDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/include/tinyxml2/CMakeFiles/tinyxml2.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jorge/Documents/GitHub/GRVC/RealSenseBoxDetection/include/tinyxml2/tinyxml2.cpp" "include/tinyxml2/CMakeFiles/tinyxml2.dir/tinyxml2.cpp.o" "gcc" "include/tinyxml2/CMakeFiles/tinyxml2.dir/tinyxml2.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
